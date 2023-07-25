282d3b930309c7453c3099d74f664a45b5f41d9c
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-25 20:14:50 +0900

    TypeORM 설정 및 env 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ormConfig } from './orm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/orm.config.ts
@@ -0,0 +1,26 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+function ormConfig(): TypeOrmModuleOptions {
+  const commonConf = {
+    SYNCRONIZE: true,
+    ENTITIES: [__dirname + '/domain/*{.ts,.js}'],
+    MIGRATIONS: [__dirname + '/migrations/**/*{.ts,.js}'],
+    MIGRATIONS_RUN: false,
+  };
+
+  return {
+    type: 'mysql',
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.PORT),
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    logging: true,
+    synchronize: commonConf.SYNCRONIZE,
+    entities: commonConf.ENTITIES,
+    migrations: commonConf.MIGRATIONS,
+    migrationsRun: commonConf.MIGRATIONS_RUN,
+  };
+}
+
+export { ormConfig };
