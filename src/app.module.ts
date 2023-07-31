8d627bc8d4abce8c2e9ef4cee5f29c025d7648f7
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-31 12:12:36 +0900

    env, module, add

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ormConfig } from './orm.config';
-
+import { ormConfig } from './_common/orm.config';
+import { MembersModule } from './members/members.module';
+import { AuthModule } from './auth/auth.module';
+import { ConcertsModule } from './concerts/concerts.module';
+import { ReservationsModule } from './reservations/reservations.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
+    MembersModule,
+    AuthModule,
+    ConcertsModule,
+    ReservationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
