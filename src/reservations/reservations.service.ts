9ff7cc5e84c2976a7bb5f4707c9d1b711879b931
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-01 13:26:06 +0900

    잔여포인트 반환 메소드 추가 및 잔여포인트 검사 검증 추가

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -1,8 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Concert } from 'src/_common/entities/concert.entity';
+import { Member } from 'src/_common/entities/member.entity';
 import { PointHistory } from 'src/_common/entities/pointHistory.entity';
 import { Reservation } from 'src/_common/entities/reservation.entity';
+import { remainingPoints } from 'src/_util/remainingPoints';
 import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
@@ -15,8 +17,11 @@ export class ReservationsService {
 
   async createReservation(concertId: number, memberId: number): Promise<{ message: string }> {
     await this.dataSource.transaction(async (manager) => {
+      const findByMember = await manager.findOne(Member, { where: { id: memberId }, relations: { points: true } });
+      const remainingPoint = remainingPoints(findByMember.points);
       const findByConcert = await manager.findOne(Concert, { where: { id: concertId } });
       if (!findByConcert) throw new NotFoundException();
+      if (remainingPoint < findByConcert.price) throw new BadRequestException('잔액이 부족합니다.');
       await manager.save(Reservation, { member: { id: memberId }, concert: { id: concertId } });
       await manager.save(PointHistory, { amount: findByConcert.price, reason: '공연 예매 차감', status: false, member: { id: memberId } });
     });
